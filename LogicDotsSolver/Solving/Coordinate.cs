a88c4c42ea33ab366891bd383e0ff29be6dfea9f
Pedro Silva <pedro.brandao.silva@gmail.com>
2016-08-01 09:39:58 +0100

    First commit, solver already working, needs a lot of refactoring and could use some optimization.

---
+++ LogicDotsSolver/Solving/Coordinate.cs
@@ -0,0 +1,45 @@
+﻿namespace LogicDotsSolver.Solving
+{
+    public struct Coordinate
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public Coordinate(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Coordinate operator +(Coordinate left, Coordinate right)
+        {
+            return new Coordinate(left.X + right.X, left.Y + right.Y);
+        }
+
+        public static Coordinate operator -(Coordinate left, Coordinate right)
+        {
+            return new Coordinate(left.X - right.X, left.Y - right.Y);
+        }
+
+        public static Coordinate operator *(Coordinate left, Coordinate right)
+        {
+            return new Coordinate(left.X * right.X, left.Y * right.Y);
+        }
+
+        public static Coordinate operator /(Coordinate left, Coordinate right)
+        {
+            return new Coordinate(left.X / right.X, left.Y / right.Y);
+        }
+
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+        }
+    }
+}
